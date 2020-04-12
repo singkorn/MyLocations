23f2be2c9fcfbfcde51fa75f2e6feb8639af22ea
Singkorn Dhepyasuvan <singkorn@gmail.com>
2020-04-12 16:08:22 +0700

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Singkorn Dhepyasuvan on 12/4/2563 BE.
+//  Copyright © 2563 Singkorn Dhepyasuvan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Singkorn Dhepyasuvan on 12/4/2563 BE.
+//  Copyright © 2563 Singkorn Dhepyasuvan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
